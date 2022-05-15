891b6990278b1a5652430105b85c7b3d9ad93766
Smacznego <smacznego@wymarzonylog.in>
2022-05-15 22:31:13 +0200

    Performance tweaks: improve emtpy pixel map, cache RGB color factors

--- src/Service/ImageGenerator.php
+++ src/Service/ImageGenerator.php
@@ -9,6 +9,7 @@ use WymarzonyLogin\PikselKapcio\ImageGeneratorConfig;
 class ImageGenerator
 {   
     private $config;
+    private $colorFactorCache;
     
     public function __construct(?ImageGeneratorConfig $config = null)
     {
@@ -17,6 +18,7 @@ class ImageGenerator
         }
         
         $this->config = $config;
+        $this->colorFactorCache = [];
     }
     
     public function generateImageData(string $text)
@@ -29,10 +31,15 @@ class ImageGenerator
         
         foreach ($pixelMap as $x => $column) {
             foreach ($column as $y => $bit) {
-                $factorRed = (int) base_convert(substr($bit, 0, 2), 16, 10);
-                $factorGreen = (int) base_convert(substr($bit, 2, 2), 16, 10);
-                $factorBlue = (int) base_convert(substr($bit, 4, 2), 16, 10);
-                imagefilledrectangle($image, $x * $scale, $y * $scale, ($x + 1) * $scale, ($y + 1) * $scale, imagecolorallocate($image, $factorRed, $factorGreen, $factorBlue));
+                if (!isset($this->colorFactorCache[$bit])) {
+                    $this->colorFactorCache[$bit] = [
+                        (int) base_convert(substr($bit, 0, 2), 16, 10),
+                        (int) base_convert(substr($bit, 2, 2), 16, 10),
+                        (int) base_convert(substr($bit, 4, 2), 16, 10),
+                    ];
+                }
+                
+                imagefilledrectangle($image, $x * $scale, $y * $scale, ($x + 1) * $scale, ($y + 1) * $scale, imagecolorallocate($image, $this->colorFactorCache[$bit][0], $this->colorFactorCache[$bit][1], $this->colorFactorCache[$bit][2]));
             }
         }
         
@@ -41,12 +48,12 @@ class ImageGenerator
     
     private function generatePixelMapForText(string $text): array
     {
-        $pixelMap = $this->initEmptyPixelMap($text);
+        $pixelMap = array_fill(0, strlen($text) * 7, array_fill(0, 7, '000000'));
                 
         foreach (str_split($text) as $characterIndex => $character) {
             $colorPair = $this->getColorPairForCharacterIndex($characterIndex);
-            $characterMap = $this->padCharacterMap($this->getCharacterMap($character));
-            
+            $characterMap = $this->getCharacterMap($character);
+                    
             foreach ($characterMap as $lineIndex => $line) {
                 $lineBitMap = str_split(str_pad(str_pad(decbin($line), 6, '0', STR_PAD_LEFT), 7, '0', STR_PAD_RIGHT));
                 
@@ -71,123 +78,101 @@ class ImageGenerator
         }
     }
     
-    private function padCharacterMap($characterMap): array
-    {
-        array_push($characterMap, 0);
-        array_unshift($characterMap, 0);
-        
-        return $characterMap;
-    }
-    
-    private function initEmptyPixelMap(string $text): array
-    {
-        $pixelMap = [];
-        
-        for ($x = 0; $x <= (strlen($text) * 7); $x++) {
-            $pixelMap[$x] = [];
-            for ($y = 0; $y <= 7; $y++) {
-                $pixelMap[$x][$y] = '000000';
-            }
-        }
-        
-        return $pixelMap;
-    }
-    
     private function getCharacterMap(string $character)
     {
         switch ($character) {
             case 'A':
-                return [14, 17, 31, 17, 17];
+                return [0, 14, 17, 31, 17, 17, 0];
             case 'B':
-                return [30, 17, 30, 17, 30];
+                return [0, 30, 17, 30, 17, 30, 0];
             case 'C':
-                return [15, 16, 16, 16, 15];
+                return [0, 15, 16, 16, 16, 15, 0];
             case 'D':
-                return [30, 17, 17, 17, 30];
+                return [0, 30, 17, 17, 17, 30, 0];
             case 'E':
-                return [31, 16, 28, 16, 31];
+                return [0, 31, 16, 28, 16, 31, 0];
             case 'F':
-                return [31, 16, 28, 16, 16];
+                return [0, 31, 16, 28, 16, 16, 0];
             case 'G':
-                return [15, 16, 19, 17, 15];
+                return [0, 15, 16, 19, 17, 15, 0];
             case 'H':
-                return [17, 17, 31, 17, 17];
+                return [0, 17, 17, 31, 17, 17, 0];
             case 'I':
-                return [4, 4, 4, 4, 4];
+                return [0, 4, 4, 4, 4, 4, 0];
             case 'J':
-                return [1, 1, 1, 17, 14];
+                return [0, 1, 1, 1, 17, 14, 0];
             case 'K':
-                return [17, 17, 30, 17, 17];
+                return [0, 17, 17, 30, 17, 17, 0];
             case 'L':
-                return [16, 16, 16, 16, 31];
+                return [0, 16, 16, 16, 16, 31, 0];
             case 'M':
-                return [10, 21, 21, 21, 21];
+                return [0, 10, 21, 21, 21, 21, 0];
             case 'N':
-                return [17, 25, 21, 19, 17];
+                return [0, 17, 25, 21, 19, 17, 0];
             case 'O':
-                return [14, 17, 17, 17, 14];
+                return [0, 14, 17, 17, 17, 14, 0];
             case 'P':
-                return [30, 17, 30, 16, 16];
+                return [0, 30, 17, 30, 16, 16, 0];
             case 'Q':
-                return [14, 17, 21, 19, 14];
+                return [0, 14, 17, 21, 19, 14, 0];
             case 'R':
-                return [30, 17, 30, 17, 17];
+                return [0, 30, 17, 30, 17, 17, 0];
             case 'S':
-                return [15, 16, 14, 1, 30];
+                return [0, 15, 16, 14, 1, 30, 0];
             case 'T':
-                return [31, 4, 4, 4, 4];
+                return [0, 31, 4, 4, 4, 4, 0];
             case 'U':
-                return [17, 17, 17, 17, 14];
+                return [0, 17, 17, 17, 17, 14, 0];
             case 'V':
-                return [17, 17, 10, 10, 4];
+                return [0, 17, 17, 10, 10, 4, 0];
             case 'W':
-                return [21, 21, 21, 21, 10];
+                return [0, 21, 21, 21, 21, 10, 0];
             case 'X':
-                return [17, 10, 4, 10, 17];
+                return [0, 17, 10, 4, 10, 17, 0];
             case 'Y':
-                return [17, 10, 4, 4, 4];
+                return [0, 17, 10, 4, 4, 4, 0];
             case 'Z':
-                return [31, 2, 4, 8, 31];
+                return [0, 31, 2, 4, 8, 31, 0];
             case '0':
-                return [14, 19, 21, 25, 14];
+                return [0, 14, 19, 21, 25, 14, 0];
             case '1':
-                return [4, 12, 4, 4, 14];
+                return [0, 4, 12, 4, 4, 14, 0];
             case '2':
-                return [14, 17, 6, 8, 31];
+                return [0, 14, 17, 6, 8, 31, 0];
             case '3':
-                return [14, 1, 6, 1, 14];
+                return [0, 14, 1, 6, 1, 14, 0];
             case '4':
-                return [17, 17, 31, 1, 1];
+                return [0, 17, 17, 31, 1, 1, 0];
             case '5':
-                return [31, 16, 30, 1, 30];
+                return [0, 31, 16, 30, 1, 30, 0];
             case '6':
-                return [14, 16, 30, 17, 14];
+                return [0, 14, 16, 30, 17, 14, 0];
             case '7':
-                return [31, 1, 2, 4, 8];
+                return [0, 31, 1, 2, 4, 8, 0];
             case '8':
-                return [14, 17, 14, 17, 14];
+                return [0, 14, 17, 14, 17, 14, 0];
             case '9':
-                return [14, 17, 15, 1, 14];
+                return [0, 14, 17, 15, 1, 14, 0];
             case '.':
-                return [0, 0, 0, 0, 4];
+                return [0, 0, 0, 0, 0, 4, 0];
             case '+':
-                return [4, 4, 31, 4, 4];
+                return [0, 4, 4, 31, 4, 4, 0];
             case '-':
-                return [0, 0, 31, 0, 0];
+                return [0, 0, 0, 31, 0, 0, 0];
             case '*':
-                return [21, 14, 4, 14, 21];
+                return [0, 21, 14, 4, 14, 21, 0];
             case '/':
-                return [1, 2, 4, 8, 16];
+                return [0, 1, 2, 4, 8, 16, 0];
             case '=':
-                return [0, 31, 0, 31, 0];
+                return [0, 0, 31, 0, 31, 0, 0];
             case '?':
-                return [14, 17, 6, 0, 4];
+                return [0, 14, 17, 6, 0, 4, 0];
             case '(':
-                return [2, 4, 4, 4, 2];
+                return [0, 2, 4, 4, 4, 2, 0];
             case ')':
-                return [8, 4, 4, 4, 8];
+                return [0, 8, 4, 4, 4, 8, 0];
             default:
-                return [0, 0, 0, 0, 0];
+                return [0, 0, 0, 0, 0, 0, 0];
         }
     }
 }
